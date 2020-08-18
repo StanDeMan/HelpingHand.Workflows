7609b62aea4fe49bb0562c1529ca797b22bfeaca
StanDeMan <StanislausE@gmx.de>
2020-08-18 14:33:24 +0200

    Remove obsolete using

--- FunctionTest.cs
+++ FunctionTest.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
